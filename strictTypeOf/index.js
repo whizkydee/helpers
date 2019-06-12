9080161ed665697ae73f36e093ecb3e961e61662
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2019-06-12 20:11:38 +0100

    Update index.js

--- strictTypeOf/index.js
+++ strictTypeOf/index.js
@@ -1,15 +1,34 @@
+const types = {
+  'string':String,
+  'array':Array,
+  'function':Function,
+  'object':Object,
+  'date':Date,
+  'boolean':Boolean,
+  'number':Number,
+  'null':null,
+  'undefined':undefined
+}
+
 const strictTypeOf = (_value, type) => {
   let value = Object(_value)
+  
+  type = types[type]
 
   if(typeof type !== 'function'){
-    if(!Array.isArray(type)){
+    if(!type || typeof type.length !== 'number'){
+      
+      if(_value === type){
+        return true
+      }
+      
       return false
     }
 
     let bitPiece = 0
     
     type.forEach( _type => {
-      bitPiece |= Number((value instanceof _type))
+      bitPiece |= Number((typeof _type === 'function' ?  (value instanceof _type) : (_value === _type)))
     })
 
     return Boolean(bitPiece)
