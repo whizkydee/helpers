d14df3a9f0c625bc48daabcb8b94adf19e29cfa5
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2019-06-12 22:17:37 +0100

    Update index.js

--- toAmountString/index.js
+++ toAmountString/index.js
@@ -1,5 +1,25 @@
 const toAmountString = amtNumeric => {
+        if(typeof amtNumeric !== 'number'){
+            return ''
+        }
+  
+        var numberText = String(amtNumeric);
+        var parts = [];
 
+        while(numberText.length >= 4){
+
+                parts = (numberText.split(/(\d{3})$/)
+                                        .reverse()
+                                            .slice(1)
+                                                .reverse())
+                                                    .concat(parts);
+                numberText = parts.shift();
+
+        }
+
+        parts.unshift(numberText);
+
+        return parts.join(',');
 }
 
 export default toAmountString
