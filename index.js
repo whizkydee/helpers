3fa67dcd25c6a879458017d6f1880403be4e01ac
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-22 00:10:50 +0100

    v0.0.5

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-import * as helpers from './src'
+/*!
+ * @mrolaolu/helpers v0.0.5
+ * https://github.com/whizkydee/helpers
+ * (c) 2019-present Olaolu Olawuyi
+ * Released under the MIT License.
+ */
 
-export default helpers
+import uniq from './src/uniq'
+import toPx from './src/toPx'
+import wait from './src/wait'
+import sleep from './src/sleep'
+import rgbToHex from './src/rgbToHex'
+import weekdays from './src/weekdays'
+import countries from './src/countries'
+import findIndex from './src/findIndex'
+import emitEvent from './src/emitEvent'
+import dotAccess from './src/dotAccess'
+import monthNames from './src/monthNames'
+import resetScroll from './src/resetScroll'
+import matchesQuery from './src/matchesQuery'
+import scrollToElem from './src/scrollToElem'
+import getEventPath from './src/getEventPath'
+import smoothScroll from './src/smoothScroll'
+import elementInView from './src/elementInView'
+import getFormFields from './src/getFormFields'
+import objectIsEmpty from './src/objectIsEmpty'
+import shortWeekdays from './src/shortWeekdays'
+import getAspectRatio from './src/getAspectRatio'
+import getFullWeekday from './src/getFullWeekday'
+import shortMonthNames from './src/shortMonthNames'
+import getShortWeekday from './src/getShortWeekday'
+import countryDialCodes from './src/countryDialCodes'
+import arrayToListItems from './src/arrayToListItems'
+import getFullMonthName from './src/getFullMonthName'
+import getShortMonthName from './src/getShortMonthName'
+import getShortMonthName from './src/getShortMonthName'
+
+export {
+  uniq,
+  toPx,
+  wait,
+  sleep,
+  rgbToHex,
+  weekdays,
+  countries,
+  emitEvent,
+  dotAccess,
+  findIndex,
+  monthNames,
+  resetScroll,
+  getEventPath,
+  scrollToElem,
+  smoothScroll,
+  matchesQuery,
+  elementInView,
+  objectIsEmpty,
+  shortWeekdays,
+  getFormFields,
+  getAspectRatio,
+  getFullWeekday,
+  shortMonthNames,
+  getShortWeekday,
+  arrayToListItems,
+  countryDialCodes,
+  getFullMonthName,
+  getShortMonthName,
+}
