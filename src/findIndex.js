659aea556cbf2962b76c1aa20cf0564f60444fa3
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-06-30 20:17:41 +0100

    Fix thisArg in findIndex polyfill

--- src/findIndex.js
+++ src/findIndex.js
@@ -2,21 +2,21 @@ export default function findIndex(array, predicate) {
   if (array == null) {
     throw new TypeError('"array" is null or not defined')
   }
-  
-  var o = Object(array)
 
-  var len = o.length >>> 0
+  let o = Object(array)
+
+  let len = o.length >>> 0
 
   if (typeof predicate !== 'function') {
     throw new TypeError('predicate must be a function')
   }
 
-  var thisArg = arguments[1]
+  let thisArg = arguments[2]
 
-  var k = 0
+  let k = 0
 
   while (k < len) {
-    var kValue = o[k]
+    let kValue = o[k]
     if (predicate.call(thisArg, kValue, k, o)) {
       return k
     }
