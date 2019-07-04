eeecbea716ecd5e94669e123caf74c5f4ceb4f4a
Uche Jude <iamuchejude@gmail.com>
2019-07-04 07:56:18 +0100

    refactor index.js file

--- index.js
+++ index.js
@@ -5,86 +5,10 @@
  * Released under the MIT License.
  */
 
-import uniq from './src/uniq'
-import toPx from './src/toPx'
-import wait from './src/wait'
-import sleep from './src/sleep'
-import safeMap from './src/safeMap'
-import safeFind from './src/safeFind'
-import debounce from './src/debounce'
-import rgbToHex from './src/rgbToHex'
-import safeSome from './src/safeSome'
-import safeSort from './src/safeSort'
-import weekdays from './src/weekdays'
-import safeEvery from './src/safeEvery'
-import countries from './src/countries'
-import emitEvent from './src/emitEvent'
-import findIndex from './src/findIndex'
-import dotAccess from './src/dotAccess'
-import safeReduce from './src/safeReduce'
-import safeFilter from './src/safeFilter'
-import monthNames from './src/monthNames'
-import safeForEach from './src/safeForEach'
-import resetScroll from './src/resetScroll'
-import matchesQuery from './src/matchesQuery'
-import scrollToElem from './src/scrollToElem'
-import getEventPath from './src/getEventPath'
-import smoothScroll from './src/smoothScroll'
-import elementInView from './src/elementInView'
-import safeFindIndex from './src/safeFindIndex'
-import getFormFields from './src/getFormFields'
-import objectIsEmpty from './src/objectIsEmpty'
-import shortWeekdays from './src/shortWeekdays'
-import getAspectRatio from './src/getAspectRatio'
-import getFullWeekday from './src/getFullWeekday'
-import safeReduceRight from './src/safeReduceRight'
-import shortMonthNames from './src/shortMonthNames'
-import getShortWeekday from './src/getShortWeekday'
-import observeMutations from './src/observeMutations'
-import countryDialCodes from './src/countryDialCodes'
-import arrayToListItems from './src/arrayToListItems'
-import getFullMonthName from './src/getFullMonthName'
-import getShortMonthName from './src/getShortMonthName'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
-export {
-  uniq,
-  toPx,
-  wait,
-  sleep,
-  rgbToHex,
-  safeMap,
-  safeFind,
-  safeSome,
-  safeSort,
-  debounce,
-  weekdays,
-  countries,
-  emitEvent,
-  dotAccess,
-  findIndex,
-  monthNames,
-  safeEvery,
-  safeReduce,
-  safeFilter,
-  safeForEach,
-  resetScroll,
-  getEventPath,
-  scrollToElem,
-  smoothScroll,
-  matchesQuery,
-  elementInView,
-  objectIsEmpty,
-  shortWeekdays,
-  getFormFields,
-  safeFindIndex,
-  getAspectRatio,
-  getFullWeekday,
-  safeReduceRight,
-  shortMonthNames,
-  getShortWeekday,
-  arrayToListItems,
-  countryDialCodes,
-  observeMutations,
-  getFullMonthName,
-  getShortMonthName,
-}
+const helpersPath = join(__dirname, './src')
+
+readFileSync(helpersPath)
+  .forEach(file => void (exports[file.split('.')[0]] = require(join(helpersPath, file))))
