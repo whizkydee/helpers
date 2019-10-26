f20e70e7cd2644277adf8f461c8728458010c3d3
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-10-26 03:59:19 +0100

    v0.2.7

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    test: {
+      presets: [
+        '@babel/env',
+        {
+          spec: true,
+          modules: false,
+          forceAllTransforms: true,
+          useBuiltIns: 'usage',
+          corejs: { version: 3, proposals: true },
+        },
+      ],
+      plugins: ['@babel/plugin-transform-runtime'],
+    },
+  },
+  presets: [
+    [
+      '@babel/env',
+      { loose: true, modules: false, },
+    ],
+  ],
+  plugins: ['@babel/plugin-proposal-object-rest-spread'],
+}
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import multiInput from 'rollup-plugin-multi-input'
+
+export default {
+  external: ['core-js'],
+  input: path.resolve(__dirname, 'src/**.js'),
+	plugins: [
+    multiInput(),
+		babel({
+			exclude: 'node_modules/**'
+		}),
+	],
+	output: [
+		{
+      format: 'es',
+			dir: 'dist/es',
+		},
+		{
+      format: 'cjs',
+			dir: 'dist/cjs',
+		}
+	]
+}
