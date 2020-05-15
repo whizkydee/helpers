01eae65e7ce869d6a317bad944e930bf5a1f682d
Olaolu Olawuyi <mrolaolu@gmail.com>
2020-05-15 17:55:14 +0100

    Use more appropriate "void" type over "undefined" for "wait" return type

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,4 @@
-export default function wait<T>(
-  timeout: number = 0,
-  cb: T
-): number | undefined {
+export default function wait<T>(timeout: number = 0, cb: T): number | void {
   if (typeof cb !== 'function') return
   return window.setTimeout(cb, timeout)
 }
