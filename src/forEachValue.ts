3b5e157e33ef133cf1dcebef206b262fdc24152e
Olaolu Olawuyi <mrolaolu@gmail.com>
2020-05-17 22:27:42 +0100

    Rename "fn" to "callback" in forEachValue to enforce consistency

--- src/forEachValue.ts
+++ src/forEachValue.ts
@@ -1,6 +1,6 @@
 export default function forEachValue<T extends { [key: string]: any }>(
   obj: T,
-  fn: (value: T[keyof T], key: keyof T, obj: T) => unknown
+  callback: (value: T[keyof T], key: keyof T, obj: T) => unknown
 ): void {
-  for (let key in obj) fn(obj[key], key, obj)
+  for (let key in obj) callback(obj[key], key, obj)
 }
