08a14d5de9993be7fab2cd2aefb5d9ce328b5b60
Olaolu Olawuyi <mrolaolu@gmail.com>
2020-08-28 01:21:08 +0100

    Add tabbable selectors

---
+++ src/tabbableSelectors.ts
@@ -0,0 +1,15 @@
+export default [
+  'a[href]:not([hidden])',
+  'area[href]:not([hidden])',
+  'button:not(:disabled):not([hidden])',
+  'embed:not([hidden])',
+  'iframe:not([hidden])',
+  'audio[controls]:not([hidden])',
+  'video[controls]:not([hidden])',
+  'input:not([type="hidden"]):not(:disabled):not([hidden])',
+  'object:not([hidden])',
+  'select:not(:disabled):not([hidden])',
+  'textarea:not(:disabled):not([hidden])',
+  '*[tabindex]:not([tabindex="-1"]):not([hidden])',
+  '*[contenteditable]:not([contenteditable="false"]):not([hidden])',
+]
